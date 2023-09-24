3ab74fecf86684c9b162228b0275c93f650ef8b0
Maurício Antunes <mauricio.abreua@gmail.com>
2023-09-24 05:16:11 -0300

    feat: Start worker implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/mauricioabreu/mosaic-video
 
 go 1.21.1
+
+require (
+	github.com/bsm/redislock v0.9.4
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/redis/go-redis/v9 v9.2.0 // indirect
+	go.uber.org/mock v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ worker/worker_test.go
@@ -0,0 +1,26 @@
+package worker_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/mauricioabreu/mosaic-video/mocks"
+	"github.com/mauricioabreu/mosaic-video/worker"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+)
+
+func TestGenerateMosaicWhenLockingFails(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	locker := mocks.NewMockLocker(ctrl)
+	urls := []string{"http://mosaicvideos.com/video1.m3u8", "http://mosaicvideos.com/video2.m3u8"}
+	locker.EXPECT().Obtain(gomock.Any(), "mosaicvideo1", gomock.Any()).Return(nil, errors.New("error obtaining lock"))
+
+	err := worker.GenerateMosaic(
+		"mosaicvideo1",
+		urls,
+		locker,
+	)
+
+	assert.NotNil(t, err)
+}
