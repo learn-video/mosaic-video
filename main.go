35fb35a6db1591d29d1b92b051e4f8cf06f03197
Maurício Antunes <mauricio.abreua@gmail.com>
2023-10-11 21:54:06 -0300

    feat: Dispatch process to generate mosaic

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
+import (
+	"github.com/bsm/redislock"
+	"github.com/mauricioabreu/mosaic-video/mosaic"
+	"github.com/mauricioabreu/mosaic-video/worker"
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
-	// urls := []string{
-	// 	"https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/level_2.m3u8",
-	// 	"https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/level_4.m3u8",
-	// }
-	// cmd := buildCommand(urls)
-	// if err := cmd.Run(); err != nil {
-	// 	log.Fatalf("Error executing ffmpeg command: %v", err)
-	// }
+	urls := []string{
+		"https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/level_2.m3u8",
+		"https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/level_4.m3u8",
+	}
+
+	rdc := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	locker := worker.NewRedisLocker(redislock.New(rdc))
+	worker.GenerateMosaic("test", urls, locker, &mosaic.FFMPEGCommand{})
 }
