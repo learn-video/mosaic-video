dd430e82a12b54bab4da12b5e9cfdea96aa80b17
Maurício Antunes <mauricio.abreua@gmail.com>
2023-10-12 08:28:45 -0300

    feat: Start using zap for logging

--- go.mod
+++ go.mod
@@ -14,5 +14,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/redis/go-redis/v9 v9.2.0 // indirect
 	go.uber.org/mock v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"github.com/mauricioabreu/mosaic-video/mosaic"
 	"github.com/mauricioabreu/mosaic-video/worker"
 	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+
 	urls := []string{
 		"https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/level_2.m3u8",
 		"https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/level_4.m3u8",
@@ -17,5 +23,7 @@ func main() {
 		Addr: "localhost:6379",
 	})
 	locker := worker.NewRedisLocker(redislock.New(rdc))
-	worker.GenerateMosaic("test", urls, locker, &mosaic.FFMPEGCommand{})
+	if err := worker.GenerateMosaic("test", urls, locker, &mosaic.FFMPEGCommand{}); err != nil {
+		sugar.Fatal(err)
+	}
 }
