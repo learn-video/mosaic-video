d94d52f8a09d6446176557769622f60a384eb151
Luis F S Covelo <luis.felipe@intelbras.com.br>
2024-01-15 22:46:14 -0300

    lint: fix gosec warning

--- internal/uploader/module.go
+++ internal/uploader/module.go
@@ -2,7 +2,9 @@ package uploader
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.uber.org/fx"
@@ -17,7 +19,19 @@ func Run(lifecycle fx.Lifecycle, handler *FileUploadHandler) {
 		OnStart: func(context.Context) error {
 			r := mux.NewRouter()
 			r.Handle("/hls/{folder}/{filename}", handler).Methods("PUT", "POST")
-			go http.ListenAndServe(":8080", r)
+
+			go func() {
+				server := &http.Server{
+					Addr:              ":8080",
+					Handler:           r,
+					ReadHeaderTimeout: time.Duration(0),
+				}
+
+				if err := server.ListenAndServe(); err != nil {
+					panic(fmt.Errorf("failed to start uploader on :8080 port. err=%v", err))
+				}
+			}()
+
 			return nil
 		},
 		OnStop: func(ctx context.Context) error {
