ff2645f8ed96256f0112619a6b746336c1cab18d
Hoega <rouxelflorent@yahoo.fr>
2024-01-17 17:10:49 +0100

    Add notEmpty tag to struct fields

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,20 +6,20 @@ import (
 
 type Config struct {
 	Redis struct {
-		Host string `env:"REDIS_HOST"`
-		Port string `env:"REDIS_PORT"`
+		Host string `env:"REDIS_HOST,notEmpty"`
+		Port string `env:"REDIS_PORT,notEmpty"`
 	}
 	API struct {
-		URL string `env:"MOSAICS_API_URL"`
+		URL string `env:"MOSAICS_API_URL,notEmpty"`
 	}
-	StaticsPath string `env:"STATICS_PATH"`
+	StaticsPath string `env:"STATICS_PATH,notEmpty"`
 	S3          struct {
-		Endpoint        string `env:"S3_ENDPOINT"`
-		AccessKeyID     string `env:"S3_ACCESS_KEY_ID"`
-		SecretAccessKey string `env:"S3_SECRET_ACCESS_KEY"`
-		BucketName      string `env:"S3_BUCKET_NAME"`
+		Endpoint        string `env:"S3_ENDPOINT,notEmpty"`
+		AccessKeyID     string `env:"S3_ACCESS_KEY_ID,notEmpty"`
+		SecretAccessKey string `env:"S3_SECRET_ACCESS_KEY,notEmpty"`
+		BucketName      string `env:"S3_BUCKET_NAME,notEmpty"`
 	}
-	UploaderEndpoint string `env:"UPLOADER_ENDPOINT"`
+	UploaderEndpoint string `env:"UPLOADER_ENDPOINT,notEmpty"`
 }
 
 func NewConfig() (*Config, error) {
