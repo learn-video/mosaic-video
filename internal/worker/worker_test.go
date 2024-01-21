f3a5d22d6835f9c43090dd714f644ed64a40e17e
Luis Felipe <lfscovelo@gmail.com>
2024-01-21 12:02:20 -0300

    test: add test to validate when create bucket fails

--- internal/worker/worker_test.go
+++ internal/worker/worker_test.go
@@ -27,10 +27,7 @@ func TestGenerateMosaicWhenLockingFails(t *testing.T) {
 	}
 	cfg := &config.Config{}
 	locker.EXPECT().Obtain(gomock.Any(), "mosaicvideo", gomock.Any()).Return(nil, errors.New("error obtaining lock"))
-
-	if cfg.StorageType.IsLocal() {
-		storage.EXPECT().CreateBucket(gomock.Any()).Return(nil)
-	}
+	storage.EXPECT().CreateBucket(gomock.Any()).Return(nil)
 
 	runningProcesses := make(map[string]bool)
 
@@ -76,10 +73,7 @@ func TestGenerateMosaicWhenExecutingCommandFails(t *testing.T) {
 	lock := mocks.NewMockLock(ctrl)
 	lock.EXPECT().Release(gomock.Any()).Return(nil)
 	locker.EXPECT().Obtain(gomock.Any(), "mosaicvideo", gomock.Any()).Return(lock, nil)
-
-	if cfg.StorageType.IsLocal() {
-		storage.EXPECT().CreateBucket(gomock.Any()).Return(nil)
-	}
+	storage.EXPECT().CreateBucket(gomock.Any()).Return(nil)
 
 	cmdExecutor := mocks.NewMockCommand(ctrl)
 	cmdExecutor.EXPECT().Execute("ffmpeg", gomock.Any()).Return(errors.New("error executing command"))
@@ -90,6 +84,48 @@ func TestGenerateMosaicWhenExecutingCommandFails(t *testing.T) {
 		cfg,
 		locker,
 		cmdExecutor,
+		runningProcesses,
+		storage,
+	)
+
+	assert.Error(t, err)
+}
+
+func TestGenerateMosaicWhenCreateBucketFails(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cfg := &config.Config{}
+	storage := mocks.NewMockStorage(ctrl)
+	mosaic := mosaic.Mosaic{
+		Name: "mosaicvideo",
+		Medias: []mosaic.Media{
+			{
+				URL: "http://example.com/mosaicvideo_1.m3u8",
+				Position: mosaic.Position{
+					X: 84,
+					Y: 40,
+				},
+				Scale: "1170x660"},
+			{
+				URL: "http://example.com/mosaicvideo_2.m3u8",
+				Position: mosaic.Position{
+					X: 1260,
+					Y: 40,
+				},
+				Scale: "568x320",
+			},
+		},
+	}
+
+	storage.EXPECT().CreateBucket(gomock.Any()).Return(errors.New("no permissions to create directory"))
+	runningProcesses := make(map[string]bool)
+
+	err := worker.GenerateMosaic(
+		mosaic,
+		cfg,
+		nil,
+		nil,
 		runningProcesses,
 		storage,
 	)
